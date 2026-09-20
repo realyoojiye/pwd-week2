e45e234c29a7d72ce7b4cf36ce01d61169095d6c
yoojiye <yoojiye2005@gmail.com>
2026-09-20 14:13:28 +0900

    Initial Commit: SvelteKit 프로젝트 생성

---
+++ src/routes/demo/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { resolve } from '$app/paths';
+	import { resolve } from '$app/paths';
+</script>
+
+<a href={resolve('/demo/playwright')}>playwright</a>
+<a href={resolve('/demo/playwright')}>playwright</a>
